7c8b6db0cc1d988085ed0d02ee588f9a3bce6191
worldkiIIer <ioo.andersson@gmail.com>
2020-05-31 01:03:47 +0200

    commit on 'master'

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ var guild;
 var general;
 
 bot.on("ready",()=>{
-    guild = bot.guilds.get("716394235212988496");
-    general = guild.channels.get("716394235212988500");
+    guild = bot.guilds.cache.get("716394235212988496");
+    general = bot.guild.channels.cache.get("716394235212988500");
     general.send("hi guys! I'm the new personal bot for this server, please give me suggestions in the server and <@400458098612895755> might implement them :)");
 });
 
