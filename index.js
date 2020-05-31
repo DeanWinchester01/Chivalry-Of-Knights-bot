c95175610edc13bb2fe79fc4aabdcd14c0cee8a4
worldkiIIer <ioo.andersson@gmail.com>
2020-05-31 14:52:59 +0200

    commit on 'master'

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var general;
 bot.on("ready",()=>{
     server = bot.guilds.cache.get("716394235212988496");
     general = server.channels.cache.get("716394235212988500");
-    general.send("You make a suggestion using the command -suggest btw");
+    general.send("Because im stuper smart ;)");
 });
 
 bot.on("message",message=>{
