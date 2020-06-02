3ba6b9539038db68cc86196007498ab3c6d3a25f
worldkiIIer <ioo.andersson@gmail.com>
2020-06-02 14:12:01 +0200

    commit on 'master'

--- index.js
+++ index.js
@@ -8,8 +8,9 @@ bot.on("ready",()=>{
     server = bot.guilds.cache.get("716394235212988496");
     general = server.channels.cache.get("716394235212988500");
     var embed = new discord.MessageEmbed()
-    .addField("```","lua\n--co",true)
-    .addField("de","```");
+    .setTitle("```lua\n--code\n```");
+    //.addField("```","lua\n--co",true)
+    //.addField("de","```");
     server.channels.cache.get("717158666125705246").send(embed);
     console.log("ready");
 });
