a858b2e2577bcb3897626c95d9b19b1005733513
worldkiIIer <ioo.andersson@gmail.com>
2020-06-02 13:53:41 +0200

    commit on 'master'

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ bot.on("ready",()=>{
     server = bot.guilds.cache.get("716394235212988496");
     general = server.channels.cache.get("716394235212988500");
     var embed = new discord.MessageEmbed()
-    .setTitle("**```lua**")
-    .setDescription("print(\"this is for a test\")\n```");
+    .setDescription("```lua\n")
+    .setDescription("--code\n```");
     server.channels.cache.get("717158666125705246").send(embed);
     console.log("ready");
 });
