8120d1b0883b352f64078e6ef4b097bee96eeabe
worldkiIIer <ioo.andersson@gmail.com>
2020-06-04 07:28:06 +0200

    commit on 'master'

--- gamebot.lua
+++
@@ -1,9 +0,0 @@
-local discordia = require("discordia")
-local client = discordia.Client()
-local token = NzE3OTcwODAyMzE3NzIxNjUy.XtiEcA.VniGY1f2FQL9mDKPJSp2UBIiQpo
-client:on("messageCreate",function(message))
-    if message.content == "test" then
-        message.channel:send("This was a success, but the WRONG FUCKING THING AND LANGUAGE")
-    end
-end)
-client:run("Bot "..token)
